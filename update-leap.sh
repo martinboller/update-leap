c335ebab0398b44221eb0911c0a22dcd5f8115b1
martinboller <martin@bollers.dk>
2019-06-17 19:42:59 +0200

    Added NTP installation

--- update-leap.sh
+++ update-leap.sh
@@ -6,9 +6,10 @@
 #                                                                   #
 # Email:        martin@bollers.dk                                   #
 # Last Update:  2019-06-17                                          #
-# Version:      1.00                                                #
+# Version:      1.10                                                #
 #                                                                   #
-# Changes:  intitial update-leap service creator                    #
+# Changes:  intitial update-leap service creator (1.00)             #
+#           Added ntp/timesyncd/dhcp functions (1.10)               #
 #                                                                   #
 # Usage:    Installs systemd timer and service                      #
 #           To update leap-seconds file at regular                  #
@@ -16,6 +17,16 @@
 #                                                                   #
 #####################################################################
 
+install_ntp() {
+    echo -e "\e[32minstall_ntp()\e[0m";
+    export DEBIAN_FRONTEND=noninteractive;
+    sudo apt-get update;
+    sudo apt-get -y install ntp;
+    sudo systemctl daemon-reload;
+    sudo systemctl enable ntp.service;
+    sudo systemctl start ntp.service;
+    /usr/bin/logger 'install_ntp()' -t 'Stratum1 NTP Server';
+}
 
 configure_update_leap() {
     echo -e "\e[32mconfigure_update-leap()\e[0m";
@@ -59,6 +70,7 @@ Unit=update-leap.service
 WantedBy=multi-user.target
 EOF";
     sync;
+    
     echo -e "\e[36m-Get initial leap file making sure timer and service can run\e[0m";
     wget -O /var/lib/ntp/leap-seconds.list http://www.ietf.org/timezones/data/leap-seconds.list;
     # Telling NTP where the leapseconds file is
@@ -72,20 +84,56 @@ EOF";
     /usr/bin/logger 'configure_update-leap()' -t 'update-leap';
 }
 
+disable_timesyncd() {
+    echo -e "\e[32mDisable_timesyncd()\e[0m";
+    sudo systemctl stop systemd-timesyncd
+    sudo systemctl daemon-reload
+    sudo systemctl disable systemd-timesyncd
+    /usr/bin/logger 'disable_timesyncd()' -t 'Stratum1 NTP Server';
+}
+
+configure_dhcp_ntp() {
+    echo -e "\e[32mconfigure_dhcp()\e[0m";
+    ## Remove ntp and timesyncd exit hooks to cater for server using DHCP
+    echo -e "\e[36m-Remove scripts utilizing DHCP\e[0m";
+    sudo rm /etc/dhcp/dhclient-exit-hooks.d/ntp
+    sudo rm /etc/dhcp/dhclient-exit-hooks.d/timesyncd
+    ## Remove ntp.conf.dhcp if it exist
+    echo -e "\e[36m-Removing ntp.conf.dhcp\e[0m";    
+    sudo rm /run/ntp.conf.dhcp
+    ## Disable NTP option for dhcp
+    echo -e "\e[36m-Disable ntp_servers option from dhclient\e[0m";   
+    sudo sed -i -e "s/option ntp_servers/#option ntp_servers/" /etc/dhcpcd.conf;
+    ## restart NTPD yet again after cleaning up DHCP
+    sudo systemctl restart ntp
+    /usr/bin/logger 'configure_dhcp()' -t 'Stratum1 NTP Server';
+}
+
 #################################################################################################################
 ## Main Routine                                                                                                 #
 #################################################################################################################
 
 main() {
-# Install NTP tools
-install_ntp_tools;
-# Create and configure systemd unit files to update leapseconds file
-configure_update_leap;
-# Add other stuff to install here as required
+    # Install NTP
+    install_ntp;
 
-## Finish with encouraging message
-echo -e "\e[32mInstallation and configuration of update-leap complete.\e[0m";
-echo -e;
+    # Install NTP tools
+    install_ntp_tools;
+
+    # Disable timesyncd to let ntp take care of time
+    disable_timesyncd;
+
+    # Ensure that DHCP does not affect ntp - do make sure that valid ntp servers are configured in ntp.conf
+    configure_dhcp_ntp:
+
+    # Create and configure systemd unit files to update leapseconds file
+    configure_update_leap;
+
+    # Add other stuff to install here as required
+
+    ## Finish with encouraging message
+    echo -e "\e[32mInstallation and configuration of NTP and update-leap complete.\e[0m";
+    echo -e;
 }
 
 main;
